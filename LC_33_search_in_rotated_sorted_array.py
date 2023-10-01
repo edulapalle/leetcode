a875e53ba54e9fcb11c1b2d1faccc502d5993614
santosh <santoshreddy.e96@gmail.com>
2023-10-01 12:15:45 -0500

    LC_33

---
+++ LC_33_search_in_rotated_sorted_array.py
@@ -0,0 +1,72 @@
+"""
+33. Search in Rotated Sorted Array
+Medium
+There is an integer array nums sorted in ascending order (with distinct values).
+
+Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
+
+Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+
+
+Example 1:
+
+Input: nums = [4,5,6,7,0,1,2], target = 0
+Output: 4
+Example 2:
+
+Input: nums = [4,5,6,7,0,1,2], target = 3
+Output: -1
+Example 3:
+
+Input: nums = [1], target = 0
+Output: -1
+
+
+Constraints:
+
+1 <= nums.length <= 5000
+-104 <= nums[i] <= 104
+All values of nums are unique.
+nums is an ascending array that is possibly rotated.
+-104 <= target <= 104
+"""
+
+#To understand this draw a sorted number line and break it at m and swap it to left and right and see the possibility of test cases ( if conditions).
+
+class Solution:
+    def search(self, nums: [int], target: int) -> int:
+        l, r = 0, len(nums) - 1
+
+        while l <= r:
+            mid = (l + r) // 2
+            if target == nums[mid]:
+                return mid
+
+            # left sorted portion
+            if nums[l] <= nums[mid]:
+                if target > nums[mid] or target < nums[l]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+            # right sorted portion
+            else:
+                if target < nums[mid] or target > nums[r]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+        return -1
+
+
+def main():
+    sol = Solution()
+    nums = [1,2,3,4,5,6,7]
+    target = 6
+    result = sol.search(nums, target)
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
