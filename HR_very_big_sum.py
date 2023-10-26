62ec9f6b5689f4eba5a03d754e217b975a2d7338
santosh <santoshreddy.e96@gmail.com>
2023-10-25 21:20:29 -0500

    HR_very_big_sum.py

---
+++ HR_very_big_sum.py
@@ -0,0 +1,38 @@
+"""
+easy
+link: https://www.hackerrank.com/challenges/a-very-big-sum/problem
+"""
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'aVeryBigSum' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts LONG_INTEGER_ARRAY ar as parameter.
+#
+
+def aVeryBigSum(ar):
+    # Write your code here
+    res = 0 # in any other programming languages, you need to used long int(), but in python int() does the same!
+    for i in ar:
+        res += i
+    return res
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = aVeryBigSum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
